c5970182205f347d9e8b80d2fa4e281de6461482
turbo-ele <eliash@gmx.at>
2015-06-12 16:50:24 +0200

    added qrl controller for parsing qr code urls

--- Resources/public/js/components/content/form/main.js
+++ Resources/public/js/components/content/form/main.js
@@ -31,16 +31,48 @@ define([
             }.bind(this));
         },
 
-        getUrl: function() {
-            // TODO: implement
-            return this.data.url;
+        /**
+         * Adds tracking parameters to url
+         *
+         * @param string url
+         *
+         * @returns string
+         */
+        addTrackingParameters: function(url) {
+            var trackingUrl,
+                divider = '?';
+            if (url.indexOf('?') > -1) {
+                divider = '&';
+            }
+            trackingUrl = url + divider + 'utm_source=qr-sulu&utm_media=qr-code&utm_campaign=' + this.data.title;
+
+            return trackingUrl;
         },
 
+        /**
+         * Get url of current page
+         *
+         * @param bool addTracking Defines if tracking params should be added
+         *
+         * @returns string
+         */
+        getUrl: function(addTracking) {
+            var baseUrl = window.location.origin,
+                pageUrl = baseUrl + '/qrl/' + this.data.id + '?locale=' + this.data.concreteLanguages[0];
+
+            return pageUrl;
+        },
+
+        /**
+         * Generate QR Code
+         *
+         * @param element
+         */
         generateQrCode: function(element) {
             new QRCode(
                 element,
                 {
-                    text: this.getUrl(),
+                    text: this.getUrl(true),
                     width: 128,
                     height: 128,
                     colorDark: "#000000",
